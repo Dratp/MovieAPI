de4d43383139b2ff79bb01c4d8e6781dc14796e0
David Heizer <DavidHeizer@QuickenLoans.com>
2020-09-22 17:22:43 -0400

    MVP achieved

---
+++ MovieAPI/Controllers/MovieController.cs
@@ -0,0 +1,53 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace MovieAPI.Controllers
+{
+    [Route("Movies")]
+    [ApiController]
+    public class MovieController : ControllerBase
+    {
+
+        //1. Get a list of all movies : Movies/All
+        [HttpGet("All")]
+        public List<Movie> All()
+        {
+            List<Movie> movies = new List<Movie>();
+            movies = Movie.ReadAll();
+            return movies;
+        }
+
+        // 2. Get a list of all movies in a specific category
+        // User specifies category as a query parameter  : Movies/Category/Parameter
+        [HttpGet("Category/{catid}")]
+        public List<Movie> CategorySearch(string catid)
+        {
+            List<Movie> movies = new List<Movie>();
+            movies = Movie.CategorySearch(catid);
+            return movies;
+        }
+
+        // 3. Get a random movie pick : Movies/Random
+        [HttpGet("Random")]
+        public Movie RandomMovie()
+        {
+            Movie movie = new Movie();
+            movie = Movie.RandomMovie();
+            return movie;
+        }
+
+        // 4. Get a random movie pick from a specific category
+        // User specifies category as a query parameter : Movies/Category/{Category varible}/RandomMovie
+        [HttpGet("{cat}/Random")]
+        public Movie CategoryRandomMovie(string cat)
+        {
+            Movie movie = new Movie();
+            movie = Movie.CategoryRandomMovie(cat);
+            return movie;
+        }
+    }
+}
---
+++ MovieAPI/Movie.cs
@@ -0,0 +1,77 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Data.SqlClient;
+using System.Linq;
+using System.Threading.Tasks;
+using Dapper;
+using Dapper.Contrib.Extensions;
+
+namespace MovieAPI
+{
+    [Table("Movie")]
+    public class Movie
+    {
+        [Key]
+        public int ID { get; set; }
+        public string Title { get; set; }
+        public string Category { get; set; }
+
+        const string server = "Server=7RP7Q13\\SQLEXPRESS;Database=Movie;user id=csharp;password=abc123"; //Dave Server
+        //const string server = "Server=7RP7Q13\\SQLEXPRESS;Database=Movie;user id=csharp;password=abc123"; //Tyler Server
+
+        public static Random rand = new Random();
+
+        public static void Create(string title, string category)
+        {
+            IDbConnection db = new SqlConnection(server);
+            Movie mov = new Movie()
+            {
+                Title = title,
+                Category = category
+            };
+            db.Insert(mov);
+        }
+
+        public static List<Movie> ReadAll()
+        {
+            IDbConnection db = new SqlConnection(server);
+            List<Movie> M = db.Query<Movie>($"select * from [Movie]").AsList<Movie>();
+            return M;
+        }
+
+        public static List<Movie> CategorySearch(string cat)
+        {
+            IDbConnection db = new SqlConnection(server);
+            List<Movie> M = db.Query<Movie>($"select * from [Movie] where Category = '{cat}'").AsList<Movie>();
+            return M;
+        }
+
+        public static List<Movie> TitleSearch(string title)
+        {
+            IDbConnection db = new SqlConnection(server);
+            List<Movie> M = db.Query<Movie>($"select * from [Movie] where Title = {title}").AsList<Movie>();
+            return M;
+        }
+
+        public static Movie RandomMovie()
+        {
+            IDbConnection db = new SqlConnection(server);
+            List<Movie> M = db.Query<Movie>($"select * from [Movie]").AsList<Movie>();
+            int result = rand.Next(0, M.Count);
+
+            return M[result];
+        }
+
+        public static Movie CategoryRandomMovie(string cat)
+        {
+            List<Movie> M = new List<Movie>();
+            M = Movie.CategorySearch(cat);
+            int result = rand.Next(0, M.Count);
+
+            return M[result];
+        }
+
+
+    }
+}
